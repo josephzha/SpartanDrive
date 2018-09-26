fa1c3fe28a036cb89737e01ec613bcaf8c5a0684
Yilin Zhao <yilinzhao@yilins-macbook-pro.local>
2018-09-26 16:41:56 -0700

    Initial Commit

---
+++ SpartanDrive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpartanDrive
+//
+//  Created by Yilin Zhao on 9/26/18.
+//  Copyright © 2018 group11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
