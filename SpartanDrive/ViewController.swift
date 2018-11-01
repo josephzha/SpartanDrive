add96b5ce9e1414b56ed6538ffc2823418f3a7d5
Pierce Tu <piercetu@gmail.com>
2018-11-01 01:48:12 -0700

    Google Segue Complete
    WIP: Google Sign out segue.
    facebook login/logout segue.\

--- SpartanDrive/ViewController.swift
+++ SpartanDrive/ViewController.swift
@@ -12,63 +12,52 @@ import Firebase
 import GoogleSignIn
 
 class ViewController: UIViewController, GIDSignInUIDelegate {
-    let image = UIImage (named: "spartan.png")
     var imageView : UIImageView!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // spartan logo
-        imageView = UIImageView(frame: CGRect(x: 163, y: 77, width: 53, height: 53))
-        imageView.contentMode = .scaleAspectFit
-        imageView.image = image
-        view.addSubview(imageView)
-        
         //fb signin button
 //        let loginButton = LoginButton(readPermissions: [ .publicProfile,.email ])
 //        loginButton.center = view.center
 //        view.addSubview(loginButton)
-    
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        
         // Google Login Segue.
-        print(GIDSignIn.sharedInstance()?.hasAuthInKeychain(), true ?? false)
-
-        if GIDSignIn.sharedInstance().hasAuthInKeychain() {
+        if (GIDSignIn.sharedInstance().hasAuthInKeychain()) {
             print("Sign In Success")
-            performSegue(withIdentifier: "SignInSegue", sender: self)
+            self.performSegue(withIdentifier: "googleSegueIn", sender: nil)
         }
-        else {
-            print("Sign In Failure")
+        
+        // Google Logout Segue.
+        if (!GIDSignIn.sharedInstance().hasAuthInKeychain()) {
+            print("Sign Out Success")
+            self.performSegue(withIdentifier: "googleSegueOut", sender: nil)
         }
         
         // Facebook Login Segue.
         
+        // Facebook Logout Segue.
     }
     
-//    @IBAction func signInSegue(_ sender: Any) {
-//        if GIDSignIn.sharedInstance().hasAuthInKeychain() {
-//            print("Sign In Success")
-//            performSegue(withIdentifier: "SignInSegue", sender: self)
-//            print("Segue Success")
-//        }
-//        else {
-//            print("Sign In Failure")
-//        }
-//    }
-    
     // Custom Google Sign In Button.
-    @IBAction func googleSignIn(_ sender: Any) {
-        GIDSignIn.sharedInstance().uiDelegate=self
+    @IBAction func googleSignIn(_ sender: UIButton) {
+        GIDSignIn.sharedInstance().uiDelegate = self
         GIDSignIn.sharedInstance().signIn()
     }
     
     // Custom Google Sign Out Button.
-    @IBAction func didTapSignOut(_ sender: UIButton) {
+    @IBAction func googleSignOut(_ sender: UIButton) {
         GIDSignIn.sharedInstance().signOut()
-        print("Sign Out Success")
     }
-    
+
     // Custom Facebook Sign In Button.
+    @IBAction func facebookSignIn(_ sender: UIButton) {
+        _ = LoginButton(readPermissions: [ .publicProfile,.email ])
+    }
     
     // Custom Facebook Sign Out Button.
     
