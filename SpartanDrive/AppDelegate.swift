3d7028f5f0ff01e61cd28ebc998f00557b387e8d
Pierce Tu <piercetu@gmail.com>
2018-11-07 15:30:16 -0800

    Fix Google Login. Able to store images on Firebase, cleaner facebook icon. Working on Progress Bar.

--- SpartanDrive/AppDelegate.swift
+++ SpartanDrive/AppDelegate.swift
@@ -5,30 +5,45 @@
 //  Created by Yilin Zhao on 9/26/18.
 //  Copyright © 2018 group11. All rights reserved.
 //
-
 import UIKit
 import FacebookCore
 import Firebase
-import FirebaseUI
 import GoogleSignIn
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Use Firebase library to configure APIs
         FirebaseApp.configure()
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
         return true
     }
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
         if let error = error {
-            print("Sign In Fail Google ", error)
+            print("Sign In Fail Google: ", error)
             return
         }
         
-        print("Sign in Success Google ", user)
+        print("Sign in Success Google: ", user)
+        
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                       accessToken: authentication.accessToken)
+        
+        Auth.auth().signInAndRetrieveData(with: credential) { (authResult, error) in
+            if let err = error {
+                print("Firebase User Create with Google Fail: ", err)
+                return
+            }
+            guard let uid = user?.userID else { return }
+            print("Firebase User Create with Google Success: ", uid)
         }
+    }
     
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
         // Perform any operations when the user disconnects from app here.
@@ -39,35 +54,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-   
+    
     @available(iOS 9.0, *)
     func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any])
         -> Bool {
-         
-        let fbHandlesSignin = SDKApplicationDelegate.shared.application(application, open: url, options: options)
-        let googleHandlesSignin = GIDSignIn.sharedInstance().handle(url as URL?,
-                                                                    sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-                                                                    annotation: options[UIApplication.OpenURLOptionsKey.annotation])
-        
-        
-        return fbHandlesSignin||googleHandlesSignin
+            return GIDSignIn.sharedInstance().handle(url,
+                                                     sourceApplication:options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+                                                     annotation: [:])
+            
     }
 }
+
 
--- SpartanDrive/googleViewController.swift
+++ SpartanDrive/googleViewController.swift
@@ -10,17 +10,115 @@ import UIKit
 import FacebookLogin
 import Firebase
 import GoogleSignIn
+import FirebaseStorage
 
-class googleViewController: UIViewController, GIDSignInUIDelegate {
+class googleViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, GIDSignInUIDelegate {
     
-    @IBOutlet weak var uploadImage: UIImageView!
-
+    @IBOutlet weak var selectedImage: UIImageView!
+    @IBOutlet weak var uploadProgressBar: UIProgressView!
+    @IBOutlet weak var uploadProgressLabel: UILabel!
+    //let your_firebase_storage_bucket = FirebaseOptions.defaultOptions()?.storageBucket ?? ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.uploadProgressBar.progress = 0.0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // Unique name for image file uploaded.
+    let filename : String = "\(NSUUID().uuidString).jpg"
+    
+    // Image Reference creates folder named "images" in Firebase Storage.
+    var imageReference: StorageReference {
+        return Storage.storage().reference().child("images")
+    }
+    
+    // Select Image from Photos.
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            selectedImage.image = image
+        }
+        else {
+            // ERROR MESSAGE
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // Moving Progress Bar Look.
+    //    @objc func movingProgressBar() {
+    //
+    //        var recievedData : Float
+    //        var expectedTotalSize : Float
+    //        var actual : Float = uploadProgressBar.progress
+    //        if (actual < 1) {
+    //            uploadProgressBar.progress = actual + (recievedData/expectedTotalSize)
+    //            Timer.scheduledTimer(timeInterval: 0.05, invocation: self, repeats: false)
+    //        }
+    //    }
+    //
+    // Upload Progress Conversion.
+    //    private func URLSession(session: URLSession, task: URLSessionTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64)
+    //    {
+    //        print("didSendBodyData")
+    //        let uploadProgress:Float = Float(totalBytesSent) / Float(totalBytesExpectedToSend)
+    //        uploadProgressBar.progress = uploadProgress
+    //        let progressPercent = Int(uploadProgress*100)
+    //        uploadProgressLabel.text = "\(progressPercent)%"
+    //        print(uploadProgress)
+    //    }
+    
+    // Select Image from Device.
+    @IBAction func selectTapped(_ sender: UIButton) {
+        let image = UIImagePickerController()
+        image.delegate = self
+        
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = false
+        
+        self.present(image, animated: true) {
+            // After Completed.
+        }
+    }
+    
+    // Upload Button
+    @IBAction func uploadTapped(_ sender: UIButton) {
+        guard let image = selectedImage.image else { return  }
+        guard let imageData = image.jpegData(compressionQuality: 1) else { return }
+        
+        let uploadImageRef = imageReference.child(filename)
+        
+        let uploadTask = uploadImageRef.putData(imageData, metadata: nil) { (metadata, error) in
+            print("UPLOAD COMPLETE")
+            print(metadata ?? "NO METADATA")
+            print(error ?? "NO ERROR")
+        }
+        
+        uploadTask.observe(.progress) { (snapshot) in
+            print(snapshot.progress ?? "NO MORE PROGRESS")
+        }
+        
+        uploadTask.resume()
+        
+        //        uploadProgressBar.setProgress(uploadTask.progress, animated: true)
+    }
+    
     // Custom Google Sign Out Button. Go back to Login page.
     @IBAction func googleSignOut(_ sender: UIButton) {
         GIDSignIn.sharedInstance().signOut()
         print("Sign Out Success")
         self.dismiss(animated: true)
     }
+    
+    
 }
 
 
